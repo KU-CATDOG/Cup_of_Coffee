2c50dcb4dd1f73f3b6d07d493ee7212f839091ed
LAPTOP-GAGC1GSJ\SAMSUNG <smin1363@korea.ac.kr>
2021-07-19 05:08:38 +0900

    Token Class 추가. Token Script, TokeTest Script 추가 Token에는 현재 Token 개수 출력, Token 개수 초기화 함수 존재 TokenTest Script에는 랜덤으로 토큰을 추가하는 예시

--- Assets/Scripts/Token.cs
+++ Assets/Scripts/Token.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class Token : MonoBehaviour
+public class Token // Token에 필요한 변수를 정의한다.
 {
+    
     public int token_happy;
     public int token_love;
     public int token_hope;
@@ -14,79 +14,43 @@ public class Token : MonoBehaviour
     public int token_tired;
     public int token_fear;
 
-    void Start()
+    public Token()
     {
-        SaveCheck();
+        token_happy = 0;
+        token_love = 0;
+        token_hope = 0;
+        token_peace = 0;
+        token_sad = 0;
+        token_anger = 0;
+        token_tired = 0;
+        token_fear = 0;
     }
 
-     void Update()
+    public void SaveCheck() // 토큰이 몇개 있는지 확인한다.
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            token_happy++;
-            PlayerPrefs.SetInt("happy", token_happy);
-            Debug.Log(token_happy);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            token_love++;
-            PlayerPrefs.SetInt("love", token_love);
-            Debug.Log(token_love);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            token_hope++;
-            PlayerPrefs.SetInt("hope", token_hope);
-            Debug.Log(token_hope);
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            token_peace++;
-            PlayerPrefs.SetInt("peace", token_peace);
-            Debug.Log(token_peace);
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            token_sad++;
-            PlayerPrefs.SetInt("sad", token_sad);
-            Debug.Log(token_sad);
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            token_anger++;
-            PlayerPrefs.SetInt("anger", token_anger);
-            Debug.Log(token_anger);
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            token_tired++;
-            PlayerPrefs.SetInt("tired", token_tired);
-            Debug.Log(token_tired);
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            token_fear++;
-            PlayerPrefs.SetInt("fear", token_fear);
-            Debug.Log(token_fear);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PlayerPrefs.DeleteKey("happy");
-        }
-
+        Debug.Log("=======================================");
+        Debug.Log("happy가" + token_happy);
+        Debug.Log("love는" + token_love);
+        Debug.Log("hope는" + token_hope);
+        Debug.Log("peace는" + token_peace);
+        Debug.Log("sad는" + token_sad);
+        Debug.Log("anger는" + token_anger);
+        Debug.Log("tired는" + token_tired);
+        Debug.Log("fear는" + token_fear);
+        Debug.Log("=======================================");
     }
-    public void SaveCheck()
+    public void Reset() // 토큰을 리셋
     {
-       
-        Debug.Log(PlayerPrefs.GetInt("happy"));
-        Debug.Log(PlayerPrefs.GetInt("love"));
-        Debug.Log(PlayerPrefs.GetInt("hope"));
-        Debug.Log(PlayerPrefs.GetInt("peace"));
-        Debug.Log(PlayerPrefs.GetInt("sad"));
-        Debug.Log(PlayerPrefs.GetInt("anger"));
-        Debug.Log(PlayerPrefs.GetInt("tired"));
-        Debug.Log(PlayerPrefs.GetInt("fear"));
+
+        token_anger = 0;
+        token_fear = 0;
+        token_happy = 0;
+        token_hope = 0;
+        token_love = 0;
+        token_peace = 0;
+       token_sad = 0;
+        token_tired = 0;
+        Debug.Log("토큰을 전부 초기화했습니다.");
 
     }
 }
