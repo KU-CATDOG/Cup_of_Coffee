6bb2c154b933daee40b45c92c690638c68e4181e
DESKTOP-LKN32VP\dalli <whsung0330@gmail.com>
2021-08-14 21:17:19 +0900

    Add character sprite and dialogue box UI

--- Assets/Scripts/CsvLoadTest.cs
+++ Assets/Scripts/CsvLoadTest.cs
@@ -8,17 +8,25 @@ using System.Text.RegularExpressions;
 
 public class CsvLoadTest : MonoBehaviour
 {
+    public GameObject DialogueBox;
     public Text ViewScript;
+    public Text NameTag;
 
-    int currentOrder = 0;    
+    public Image CharacterSprite;
+    private TextAsset[] NPCscripts;
 
     GameTime gameTime;
     int dayCount = 0;
+
+    int currentOrder = 0;    
+
+
 
     public class scripts{   //class to store each script
         public int day;
         public int order;
         public string script;
+        public string name;
     }
 
     List<scripts> inGameScripts = new List<scripts>(); //list to store all scripts
@@ -42,21 +50,17 @@ public class CsvLoadTest : MonoBehaviour
         }
     }
 
+
     void LoadCSV(){
-        TextAsset scriptResource = Resources.Load<TextAsset>("점장 대사");
-        TextAsset scriptResource2 = Resources.Load<TextAsset>("카페알바생 대사");
 
-        string [] data = scriptResource.text.Split(new char[] { '\n' });
-        string [] data2 = scriptResource2.text.Split(new char[] { '\n' });
+        NPCscripts = Resources.LoadAll<TextAsset>("CharacterScripts");
+        foreach(var script in NPCscripts){
+            string [] data = script.text.Split(new char [] { '\n' });
 
-        for(int i=0; i<data.Length-1; i++){
-            var singleData = Regex.Split(data[i], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-            inGameScripts.Add(new scripts{day = int.Parse(singleData[0]), order = int.Parse(singleData[1]), script = singleData[2].Trim('"')});
-        }
-
-        for(int i=0; i<data2.Length-1; i++){
-            var singleData = Regex.Split(data2[i], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-            inGameScripts.Add(new scripts{day = int.Parse(singleData[0]), order = int.Parse(singleData[1]), script = singleData[2].Trim('"')});
+            for(int i=0; i<data.Length-1; i++){
+                var singleData = Regex.Split(data[i], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                inGameScripts.Add(new scripts{day = int.Parse(singleData[0]), order = int.Parse(singleData[1]), script = singleData[2].Trim('"'), name = script.name.Split(' ')[0]});
+            }
         }
 
 
@@ -66,15 +70,18 @@ public class CsvLoadTest : MonoBehaviour
         dayCount = gameTime.day;
 
         if(currentOrder >= Sorted.Count){
+            DialogueBox.SetActive(false);
             return;
         }
 
         if(dayCount != Sorted[currentOrder].day){
-            ViewScript.gameObject.SetActive(false);
+            DialogueBox.SetActive(false);
             return;
         }
         else{
-            ViewScript.gameObject.SetActive(true);
+            DialogueBox.SetActive(true);
+
+            MatchCharacter(Sorted[currentOrder].name);
             ViewScript.text = Sorted[currentOrder].script;
             currentOrder++;
         }
@@ -83,6 +90,30 @@ public class CsvLoadTest : MonoBehaviour
 
     void SortScripts(){
         Sorted = inGameScripts.OrderBy(x=> x.day).ThenBy(x=> x.order).ToList();
+    }
+
+    void MatchCharacter(string name){
+        NameTag.text = name;
+        switch(name){
+            case "점장":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/점장");
+                break;
+            case "카페알바생":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/알바생2");
+                break;
+            case "감시자":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/감시자");
+                break;
+            case "반란군":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/반란군");
+                break;
+            case "브로커":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/브로커");
+                break;
+            case "상관":
+                CharacterSprite.sprite = Resources.Load<Sprite>("CharacterSprites/상관");
+                break;
+        }
     }
     
 }
