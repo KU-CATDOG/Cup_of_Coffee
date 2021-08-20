a87bc2c70ed8804c4fd397180987d57d206c4444
DESKTOP-LKN32VP\dalli <whsung0330@gmail.com>
2021-08-20 22:21:40 +0900

    place cups when pulling shots from espresso machine

--- Assets/Scripts/EspressoMachine.cs
+++ Assets/Scripts/EspressoMachine.cs
@@ -15,6 +15,11 @@ public class EspressoMachine : MonoBehaviour
     float shottimingRight = 0;
     float fill = 0;
 
+    bool cupLeft1 = false;
+    bool cupLeft2 = false;
+    bool cupRight1 = false;
+    bool cupRight2 = false;
+
     public GameObject Left1;
     public GameObject Left2;
     public GameObject Left3;
@@ -31,21 +36,71 @@ public class EspressoMachine : MonoBehaviour
         recipe = GameObject.Find("RecipeTest").GetComponent<Recipe>();
     }
 
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Z)){    //(임시) Z 누르면 컵 제거
+            removeCupLeft();
+            removeCupRight();
+        }
+    }
+
     #region SingleShot
+
+    public bool placeCupLeft(){
+        if(!cupLeft1){
+            cupLeft1 = true;
+            Debug.Log("First cup placed on the left side");
+            return true;
+        }
+        else if(cupLeft1 && !cupLeft2){
+            cupLeft2 = true;
+            Debug.Log("Second cup placed on the left side");
+            return true;
+        }
+        else {
+            Debug.Log("Remove cups before pulling another shot");
+            return false;               //컵 2개가 이미 놓여있을 경우 false
+        }
+    }
+
+    public void removeCupLeft(){ // cupLeft2 -> 1 순으로 컵 제거
+        if(shotpullingLeft){        //샷 내리는 중 컵 제거하면 샷도 취소
+            shotpullingLeft= false;
+            loadingLeft.fillAmount = 1;
+            loadingLeft.color = new Color32(200, 200, 200, 128);
+        }
+        if(cupLeft2){
+            cupLeft2 = false;
+            Debug.Log("Second cup removed (Left)");
+        }
+        else if(cupLeft1){
+            cupLeft1 = false;
+            Debug.Log("First cup removed (Left)");
+        }
+        else{
+            Debug.Log("No cup to remove (Left)");
+        }
+    }
+
+    
     public void SingleRistretto()
     {
+        
         if (!shotpullingLeft)
         {
-            loadingLeft = Left1.GetComponent<Image>();
-            shotpullingLeft = true;
-            loadingLeft.color = new Color32(158, 222, 115, 255);
-            StartCoroutine(shotTimingLeft(ristretto, loadingLeft));
+            if(placeCupLeft()){
+                loadingLeft = Left1.GetComponent<Image>();
+                shotpullingLeft = true;
+                loadingLeft.color = new Color32(158, 222, 115, 255);
+                StartCoroutine(shotTimingLeft(ristretto, loadingLeft));
+            }
         }
         else
         {
             shotpullingLeft = false;
             loadingLeft.fillAmount = 1;
             loadingLeft.color = new Color32(200, 200, 200, 128);
+            removeCupLeft();
+            
         }
 
     }
@@ -56,16 +111,20 @@ public class EspressoMachine : MonoBehaviour
 
         if (!shotpullingLeft)
         {
-            loadingLeft = Left2.GetComponent<Image>();
-            shotpullingLeft = true;
-            loadingLeft.color = new Color32(247, 234, 0, 255);
-            StartCoroutine(shotTimingLeft(espresso, loadingLeft));
+            if(placeCupLeft()){
+                loadingLeft = Left2.GetComponent<Image>();
+                shotpullingLeft = true;
+                loadingLeft.color = new Color32(247, 234, 0, 255);
+                StartCoroutine(shotTimingLeft(espresso, loadingLeft));
+            }
         }
         else
         {
             shotpullingLeft = false;
             loadingLeft.fillAmount = 1;
             loadingLeft.color = new Color32(200, 200, 200, 128);
+            removeCupLeft();
+            
         }
     }
 
@@ -74,22 +133,39 @@ public class EspressoMachine : MonoBehaviour
 
         if (!shotpullingLeft)
         {
-            loadingLeft = Left3.GetComponent<Image>();
-            shotpullingLeft = true;
-            loadingLeft.color = new Color32(228, 137, 0, 255);
-            StartCoroutine(shotTimingLeft(lungo, loadingLeft));
+            if(placeCupLeft()){
+                loadingLeft = Left3.GetComponent<Image>();
+                shotpullingLeft = true;
+                loadingLeft.color = new Color32(228, 137, 0, 255);
+                StartCoroutine(shotTimingLeft(lungo, loadingLeft));
+            }
         }
         else
         {
             shotpullingLeft = false;
             loadingLeft.fillAmount = 1;
             loadingLeft.color = new Color32(200, 200, 200, 128);
+            removeCupLeft();
+            
         }
     }
 
 
     IEnumerator shotTimingLeft(float EspressoType, Image loading)
     {
+        // if(cupLeft1 && cupLeft2){
+        //     Debug.Log("remove cups before pulling another shot!");
+        //     yield break;
+        // }
+        // else if(cupLeft1){
+        //     cupLeft2 = true;
+        //     Debug.Log("Second cup placed on the left side");
+        // }
+        // else{
+        //     cupLeft1 = true;
+        //     Debug.Log("First cup placed on the left side");
+        // }
+
         shottimingLeft = 0;
 
         while (shotpullingLeft && shottimingLeft < EspressoType)
@@ -127,21 +203,54 @@ public class EspressoMachine : MonoBehaviour
     #endregion
 
     #region DoubleShot
+
+    public bool placeCupRight(){
+        if(!cupRight1 && !cupRight2){
+            cupRight1 = true;
+            cupRight2 = true;
+            Debug.Log("Both cups placed on the right side");
+            return true;
+        }
+        else{
+            Debug.Log("Remove cups before pulling another shot");
+            return false;
+        }
+    }
+
+    public void removeCupRight(){ 
+        if(shotpullingRight){        
+            shotpullingRight= false;
+            loadingRight.fillAmount = 1;
+            loadingRight.color = new Color32(200, 200, 200, 128);
+        }
+        if(cupRight1 && cupRight1){
+            cupRight1 = false;
+            cupRight2 = false;
+            Debug.Log("Both cups removed on the right side");
+        }
+        else{
+            Debug.Log("No cup to remove (Right)");
+        }
+    }
+
     public void DoubleRistretto()
     {
 
         if (!shotpullingRight)
         {
-            loadingRight = Right1.GetComponent<Image>();
-            shotpullingRight = true;
-            loadingRight.color = new Color32(158, 222, 115, 255);
-            StartCoroutine(shotTimingRight(ristretto, loadingRight));
+            if(placeCupRight()){
+                loadingRight = Right1.GetComponent<Image>();
+                shotpullingRight = true;
+                loadingRight.color = new Color32(158, 222, 115, 255);
+                StartCoroutine(shotTimingRight(ristretto, loadingRight));
+            }
         }
         else
         {
             shotpullingRight = false;
             loadingRight.fillAmount = 1;
             loadingRight.color = new Color32(200, 200, 200, 128);
+            removeCupRight();
         }
 
     }
@@ -151,16 +260,19 @@ public class EspressoMachine : MonoBehaviour
 
         if (!shotpullingRight)
         {
-            loadingRight = Right2.GetComponent<Image>();
-            shotpullingRight = true;
-            loadingRight.color = new Color32(247, 234, 0, 255);
-            StartCoroutine(shotTimingRight(espresso, loadingRight));
+            if(placeCupRight()){
+                loadingRight = Right2.GetComponent<Image>();
+                shotpullingRight = true;
+                loadingRight.color = new Color32(247, 234, 0, 255);
+                StartCoroutine(shotTimingRight(espresso, loadingRight));
+            }
         }
         else
         {
             shotpullingRight = false;
             loadingRight.fillAmount = 1;
             loadingRight.color = new Color32(200, 200, 200, 128);
+            removeCupRight();
         }
     }
 
@@ -169,16 +281,19 @@ public class EspressoMachine : MonoBehaviour
 
         if (!shotpullingRight)
         {
-            loadingRight = Right3.GetComponent<Image>();
-            shotpullingRight = true;
-            loadingRight.color = new Color32(228, 137, 0, 255);
-            StartCoroutine(shotTimingRight(lungo, loadingRight));
+            if(placeCupRight()){
+                loadingRight = Right3.GetComponent<Image>();
+                shotpullingRight = true;
+                loadingRight.color = new Color32(228, 137, 0, 255);
+                StartCoroutine(shotTimingRight(lungo, loadingRight));
+            }
         }
         else
         {
             shotpullingRight = false;
             loadingRight.fillAmount = 1;
             loadingRight.color = new Color32(200, 200, 200, 128);
+            removeCupRight();
         }
     }
 
