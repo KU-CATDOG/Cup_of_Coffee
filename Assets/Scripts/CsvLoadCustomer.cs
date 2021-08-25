756ec1b6f04dcf081dabea56e84364a1a8cf0c3e
DESKTOP-LKN32VP\dalli <whsung0330@gmail.com>
2021-08-25 13:45:25 +0900

    Add customer and emotion sprites

--- Assets/Scripts/CsvLoadCustomer.cs
+++ Assets/Scripts/CsvLoadCustomer.cs
@@ -25,6 +25,9 @@ public class CsvLoadCustomer : MonoBehaviour
     public int real; // real = 진짜 감정을 줄 확률
     public int customertoken; //손님이 들어올 때 결정되는 토큰
 
+    public Image CustomerSprite;
+    public Image RealEmotionSprite;
+
     void Start()
     {
         isActive = false;
@@ -42,6 +45,9 @@ public class CsvLoadCustomer : MonoBehaviour
         Debug.Log("SetRandom()");
         menu(menunumber);
         customertoken = Random.Range(1, 8);
+
+        LoadCustomerSprite(); //손님 스프라이트
+        LoadEmotionSprite(customertoken);  //감정 스프라이트 로드
     }
 
 
@@ -50,6 +56,7 @@ public class CsvLoadCustomer : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             Customer();
+            
         }
 
 
@@ -75,26 +82,41 @@ public class CsvLoadCustomer : MonoBehaviour
     {
         if (isActive == true)
         {
-            if (currentorder == 2)
+
+            Customertext.text = text[currentorder];
+            Debug.Log(currentorder);
+            if(currentorder < 3) // 자꾸 배열 크기 오류 나서 이렇게 설정
             {
-                //isActive = false;
+                currentorder++;
+            }
+
+            if (currentorder == 1)
+            {
+
                 if (isLock == false) // 반복해서 주문이 오지 않도록 설정
                 {
                     SetRandom();
                     isLock = true;
+
+                    return;
                 }
-                return;
             }
 
-            Customertext.text = text[currentorder];
-            currentorder++;
-
         }
+    }
+
+    public void CustomerReset()
+    {
+        currentorder = 0;
+        isLock = false;
+        isSuccess = false;
+        isFail = false;
     }
 
     public void Pass()
     {
         Customertext.text = "감사합니다. 수고하세요~";
+        CustomerReset();
         int random = Random.Range(0, 100);
         if (random < real) // 진짜 감정을 줄 때
         {
@@ -110,9 +132,11 @@ public class CsvLoadCustomer : MonoBehaviour
     public void Fail()
     {
         Customertext.text = "아니 이걸 시킨 적이 없는데... 이게 왜 나와요;;";
+        CustomerReset();
+
     }
 
-    public void menu(int number)
+    public void menu(int number) // 메뉴 호출
     {
         switch (number)
         {
@@ -131,14 +155,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "espresso";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "에스프레소로 시지 않게 한 잔 주세요.";
                     menustring = "espresso_lungo";
                     menunumber = 31;
                     break;
                 }
-                break;
 
             case 2:
                 b = Random.Range(1, 100);
@@ -155,14 +178,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caramel_macchiato_ice";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "시원한 카라멜 마끼아또로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caramel_macchiato_ice_lungo";
                     menunumber = 33;
                     break;
                 }
-                break;
             case 3:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -178,14 +200,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caramel_macchiato_hot";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 카라멜 마끼아또로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caramel_macchiato_hot_lungo";
                     menunumber = 35;
                     break;
                 }
-                break;
             case 4:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -201,14 +222,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "americano_hot";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 아메리카노로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "americano_hot_lungo";
                     menunumber = 39;
                     break;
                 }
-                break;
             case 5:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -224,14 +244,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "americano_ice";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "아이스 아메리카노로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "americano_ice_lungo";
                     menunumber = 37;
                     break;
                 }
-                break;
             case 6:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -247,14 +266,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caffe_mocha_ice";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "아이스 카페모카로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caffe_mocha_ice_lungo";
                     menunumber = 41;
                     break;
                 }
-                break;
             case 7:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -270,14 +288,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caffe_mocha_hot";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 카페모카로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caffe_mocha_hot_lungo";
                     menunumber = 43;
                     break;
                 }
-                break;
             case 8:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -293,14 +310,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "espresso_con_panna";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "에스프레소 콘 파냐로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "espresso_con_panna_lungo";
                     menunumber = 45;
                     break;
                 }
-                break;
             case 9:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -316,14 +332,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "espresso_macchiato";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "에스프레소 마끼아또로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "espresso_macchiato_lungo";
                     menunumber = 47;
                     break;
                 }
-                break;
             case 10:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -339,14 +354,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "cappuccino";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "카푸치노로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "cappuccino_lungo";
                     menunumber = 49;
                     break;
                 }
-                break;
             case 11:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -362,14 +376,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "vanilla_latte_hot";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 바닐라 라떼로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "vanilla_latte_hot_lungo";
                     menunumber = 51;
                     break;
                 }
-                break;
             case 12:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -385,14 +398,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "vanilla_latte_ice";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 바닐라 라떼로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "vanilla_latte_ice_lungo";
                     menunumber = 53;
                     break;
                 }
-                break;
             case 13:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -408,14 +420,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caffe_latte_hot";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "따뜻한 카페 라떼로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caffe_latte_hot_lungo";
                     menunumber = 55;
                     break;
                 }
-                break;
             case 14:
                 b = Random.Range(1, 100);
                 if (0 <= b && b < 15)
@@ -431,14 +442,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "caffe_latte_ice";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "시원한 카페 라떼로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "caffe_latte_ice_lungo";
                     menunumber = 57;
                     break;
                 }
-                break;
             case 15:
                 text[1] = "아이스 녹차 라떼로 한 잔 주세요.";
                 menustring = "greentea_latte_ice";
@@ -498,14 +508,13 @@ public class CsvLoadCustomer : MonoBehaviour
                     menustring = "espresso_frapp";
                     break;
                 }
-                else if (b >= 85)
+                else
                 {
                     text[1] = "에스프레소 프라푸치노로 한 잔 주세요. 시지 않게 해서 주세요.";
                     menustring = "espresso_frapp_lungo";
                     menunumber = 59;
                     break;
                 }
-                break;
             case 27:
                 text[1] = "녹차 프라푸치노 한 잔 주세요.";
                 menustring = "greentea_frapp";
@@ -524,5 +533,42 @@ public class CsvLoadCustomer : MonoBehaviour
 
 
 
+    } 
+
+    void LoadCustomerSprite(){
+        int i = Random.Range(1,24);
+        CustomerSprite.sprite = Resources.Load<Sprite>("CustomerSprites/손님" + i);
+        CustomerSprite.gameObject.SetActive(true);
+    }
+
+    void LoadEmotionSprite(int customertoken){
+        switch(customertoken){
+            case 1:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/행복");
+                break;
+            case 2:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/사랑");
+                break;
+            case 3:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/기대");
+                break;
+            case 4:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/평온");
+                break;
+            case 5:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/슬픔");
+                break;
+            case 6:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/분노");
+                break;
+            case 7:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/피로");
+                break;
+            case 8:
+                RealEmotionSprite.sprite = Resources.Load<Sprite>("EmotionSprites/공포");
+                break;
+
+        }
+        RealEmotionSprite.gameObject.SetActive(true);
     }
 }
